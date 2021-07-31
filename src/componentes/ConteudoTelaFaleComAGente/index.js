ac2e817875c56c9e20ba33913f98b6f04a9739e1
Luiz Júnior Lopes <55715995+fenix26@users.noreply.github.com>
2021-07-30 22:14:51 -0300

    links abre eno outra aba

--- src/componentes/ConteudoTelaFaleComAGente/index.js
+++ src/componentes/ConteudoTelaFaleComAGente/index.js
@@ -18,14 +18,14 @@ function ConteudoTelaFaleComAGente() {
                         <h3>
                             <ul>
                                 <p>O conteúdo apresentado na plataforma "Poderosa no Parto" está baseado no curso
-                                    <a className="a2" href="http://nepae.uff.br/2020/07/08/preparo-para-o-parto-e-aleitamento/"> Preparo para o Parto e Aleitamento</a>,
+                                    <a className="a2" href="http://nepae.uff.br/2020/07/08/preparo-para-o-parto-e-aleitamento/" target="_blank"> Preparo para o Parto e Aleitamento</a>,
                                 oferecido pelo NEPAE - Núcleo de Estudos e Pesquisas sobre as Atividades de Enfermagem e pelo NESEN
                                  - Núcleo  de Estudos sobre Saúde e Etnia Negra, da UFF - Universidade Federal Fluminense.
 
                                     </p>
 
                                 <p>É possível entrar em contato com a coordenadora através do portal eletrônico do NEPAE {"&"} NESEN. Ao final da
-                                <a className="a2" href="http://nepae.uff.br/"> página inicial</a> você encontrará um campo para enviar mensagens, dúvidas e sugestões. Não se esqueça de indicar o assunto.
+                                <a className="a2" href="http://nepae.uff.br/" target="_blank"> página inicial</a> você encontrará um campo para enviar mensagens, dúvidas e sugestões. Não se esqueça de indicar o assunto.
                                  </p>
 
 
